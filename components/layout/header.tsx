f1b7a1f4a2cf1c5236363e5d02ef7bcb303fddf7
Pedro Souza <shiro2303@gmail.com>
2025-08-22 18:30:43 -0300

    fix: update header links to point to the home page

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -55,7 +55,7 @@ export function Header({ minimal = false }: HeaderProps) {
     >
       <div className="flex items-center">
         <div className="md:hidden flex items-center">
-          <Link href="#" className="flex items-center space-x-2">
+          <Link href="/" className="flex items-center space-x-2">
             <Image
               src="/assets/logo.svg"
               alt="NeuronHire Logo"
@@ -70,7 +70,7 @@ export function Header({ minimal = false }: HeaderProps) {
         <div
           className={`hidden md:flex items-center rounded-full px-3 py-2 space-x-6 bg-white`}
         >
-          <Link href="#">
+          <Link href="/">
             <div className="flex items-center space-x-2 pr-4">
               <Image
                 src="/assets/logo.svg"

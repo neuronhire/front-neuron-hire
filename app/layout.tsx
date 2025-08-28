e4b406d729b37ea6e6ceae00d1836b82e87603c2
Pedro Souza <shiro2303@gmail.com>
2025-08-28 01:07:03 -0300

    fix: add Google Analytics integration and update dependencies for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -107,6 +108,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
       </body>
     </html>
   );
